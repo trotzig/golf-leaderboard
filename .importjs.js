5aa28a906074e8770f601731cf07f3c445427ed7
Henric Trotzig <henric.trotzig@gmail.com>
2022-02-18 11:27:11 +0100

    Show leaderboard
    Barebones right now, but there's data!

---
+++ .importjs.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+module.exports = {
+  declarationKeyword({ pathToCurrentFile }) {
+    if (
+      pathToCurrentFile.endsWith('-happo.js') ||
+      pathToCurrentFile.endsWith('-test.js') ||
+      /\/pages\//.test(pathToCurrentFile)
+    ) {
+      return 'import';
+    }
+
+    try {
+      if (/(export |import )/.test(fs.readFileSync(pathToCurrentFile))) {
+        return 'import';
+      }
+    } catch (e) {}
+    return 'const';
+  },
+  namedExports: {
+    react: ['useEffect', 'useState', 'useRef'],
+  },
+  globals: ['console', 'document'],
+  logLevel: 'debug',
+  excludes: [
+    './.next/**',
+    './.cron-logs/**',
+  ],
+  ignorePackagePrefixes: ['react-'],
+};
---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+
+function getEntries(data) {
+  const classKey = Object.keys(data.Classes)[0];
+  const entries = data.Classes[classKey].Leaderboard.Entries;
+  const entryKeys = Object.keys(entries);
+  return entryKeys.map(key => entries[key]);
+}
+
+export default function StartPage() {
+  const [data, setData] = useState();
+  console.log(data);
+  useEffect(() => {
+    const rndFunctionName = `rnd_${Math.floor(Math.random() * 1000001)}`;
+    window[rndFunctionName] = payload => {
+      setData(payload);
+    };
+    const scriptEl = document.createElement('script');
+    const url = `https://scores.golfbox.dk/Handlers/LeaderboardHandler/GetLeaderboard/CompetitionId/3176088/ClassId/3066464/RoundNumber/0/language/2057/?callback=${rndFunctionName}&_=1645174112687`;
+    scriptEl.src = url;
+    document.body.appendChild(scriptEl);
+  }, []);
+  const entries = data && getEntries(data);
+  return (
+    <div className="leaderboard">
+      <h2>Leaderboard</h2>
+      <ul>
+        {entries &&
+          entries.map(entry => {
+            return (
+              <li key={entry.RefID}>
+                <span>{entry.Position.Calculated}</span>
+                <span>
+                  {entry.FirstName} {entry.LastName}
+                </span>
+                <span className="to-par">{entry.ResultSum.ToParText}</span>
+                <span className="club">{entry.ClubName}</span>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+}
