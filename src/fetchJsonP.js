e9cfe4bafbe395ddc92c7cff0911237cafa5e628
Henric Trotzig <henric.trotzig@gmail.com>
2022-02-25 16:30:35 +0100

    Make jsonp urls deterministic in nature
    I was hoping this would make caching work. But it turns out they set
    no-cache headers. Anyway, feels good to have urls consistently produced.

--- src/fetchJsonP.js
+++ src/fetchJsonP.js
@@ -1,11 +1,15 @@
+import md5 from 'crypto-js/md5';
+
+const loadTime = Date.now();
+
 export default function fetchJsonP(url) {
   return new Promise(resolve => {
-    const rndFunctionName = `rnd_${Math.floor(Math.random() * 1000001)}`;
+    const rndFunctionName = `cb_${md5(url)}`;
     window[rndFunctionName] = payload => {
       resolve(payload);
     };
     const scriptEl = document.createElement('script');
-    scriptEl.src = `${url}?callback=${rndFunctionName}&_=${Date.now()}`;
+    scriptEl.src = `${url}?callback=${rndFunctionName}&_=${loadTime}`;
     document.body.appendChild(scriptEl);
   });
 }
