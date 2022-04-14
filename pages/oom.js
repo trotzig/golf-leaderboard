edd7275c59c0d3ce29d369d6a87cb080aca1d584
Henric Trotzig <henric.trotzig@gmail.com>
2022-04-14 14:58:20 +0200

    Add link to player from oom

--- pages/oom.js
+++ pages/oom.js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import React, { useState } from 'react';
 
 import { useJsonPData } from '../src/fetchJsonP';
 import FavoriteButton from '../src/FavoriteButton';
 import LoadingSkeleton from '../src/LoadingSkeleton';
 import Menu from '../src/Menu';
+import generateSlug from '../src/generateSlug';
 
 const NUM_FORMATTER = Intl.NumberFormat('en-US', {
   notation: 'compact',
@@ -30,39 +32,43 @@ function getEntries(data) {
 function Player({ entry, onFavorite }) {
   const classes = ['player'];
   return (
-    <li className={classes.join(' ')}>
-      <span className="position">
-        <span>{entry.Position}</span>
-        <FavoriteButton playerId={entry.MemberID} onChange={onFavorite} />
-      </span>
-      <span>
-        {entry.FirstName} {entry.LastName}
-        <br />
-        <span className="club">{entry.ClubName}</span>
-      </span>
-      <span className="score">
-        {NUM_FORMATTER.format(Math.round(entry.CalculatedResult))}
-      </span>
-      <span className="stats">
-        <div className="round">
-          {Object.values(entry.Results).map(result => {
-            return (
-              <div
-                key={result.CompetitionID}
-                className={`round-score ${
-                  isFirst(result.Position)
-                    ? 'first'
-                    : isTop10(result.Position)
-                    ? 'top-10'
-                    : ''
-                }`}
-              >
-                {result.Result > 0 ? result.Position : '—'}
-              </div>
-            );
-          })}
-        </div>
-      </span>
+    <li>
+      <Link href={`/${generateSlug(entry)}`}>
+        <a className={classes.join(' ')}>
+          <span className="position">
+            <span>{entry.Position}</span>
+            <FavoriteButton playerId={entry.MemberID} onChange={onFavorite} />
+          </span>
+          <span>
+            {entry.FirstName} {entry.LastName}
+            <br />
+            <span className="club">{entry.ClubName}</span>
+          </span>
+          <span className="score">
+            {NUM_FORMATTER.format(Math.round(entry.CalculatedResult))}
+          </span>
+          <span className="stats">
+            <div className="round">
+              {Object.values(entry.Results).map(result => {
+                return (
+                  <div
+                    key={result.CompetitionID}
+                    className={`round-score ${
+                      isFirst(result.Position)
+                        ? 'first'
+                        : isTop10(result.Position)
+                        ? 'top-10'
+                        : ''
+                    }`}
+                  >
+                    {result.Result > 0 ? result.Position : '—'}
+                  </div>
+                );
+              })}
+            </div>
+          </span>
+        </a>
+      </Link>
     </li>
   );
 }
