ff4b2b6010e9e6232df91b1ea71bd39a414b9306
Henric Trotzig <henric.trotzig@gmail.com>
2023-02-27 12:48:49 +0100

    Make comp slug unique and required
    So that we can use it in URLs

--- src/generateCompetitionSlug.mjs
+++ src/generateCompetitionSlug.mjs
@@ -1,10 +1,19 @@
+import { getYear } from 'date-fns';
+
 export default function generateCompetitionSlug(comp) {
   const name = comp.name || comp.Name;
-  return name
+  const year = getYear(comp.start || comp.StartDate);
+  const slug = name
     .toLowerCase()
     .replace(/\s+/g, '-')
     .replace(/[åäæ]/g, 'a')
     .replace(/[öø]/g, 'o')
     .replace(/é/, 'e')
-    .replace(/[^a-z-]/g, '');
+    .replace(/[^a-z0-9-]/g, '');
+
+  if (/[0-9]{4}$/.test(slug)) {
+    // year already part of slug
+    return slug;
+  }
+  return `${slug}-${year}`;
 }
