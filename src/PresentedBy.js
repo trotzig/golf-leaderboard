e46c5fbb530226d50320b87e9831f59f1639c591
Henric Trotzig <henric.trotzig@gmail.com>
2023-03-03 13:52:31 +0100

    Add presented-by explanation text
    Also introduce the font that Stripe uses to make it pop more in the
    layout.

--- src/PresentedBy.js
+++ src/PresentedBy.js
@@ -5,14 +5,30 @@ import StripeLogoSvg from './StripeLogoSvg.js';
 export default function PresentedBy() {
   return (
     <a href="https://stripegolf.se/" className="presented-by">
-      <div className="presented-by-intro">Presented by</div>
+      <div className="presented-by-intro">Presented by stripegolf.se</div>
       <div className="presented-by-main">
         <StripeLogoSvg />
         <div className="presented-by-products">
-          <img src="/presented-by/putter-headcover.png" width="300" height="284"/>
-          <img src="/presented-by/golfballs_green.png" width="300" height="185" />
-          <img src="/presented-by/putter-headcover2.png" width="300" height="253" />
+          <img
+            src="/presented-by/putter-headcover.png"
+            width="300"
+            height="284"
+          />
+          <img
+            src="/presented-by/golfballs_green.png"
+            width="300"
+            height="185"
+          />
+          <img
+            src="/presented-by/putter-headcover2.png"
+            width="300"
+            height="253"
+          />
         </div>
+      </div>
+      <div className="presented-by-outro">
+        Golfbollar, headcovers och annat som gör golfbagen snyggare och spelet
+        enklare.
       </div>
     </a>
   );
