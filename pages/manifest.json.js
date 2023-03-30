acb67fe958fe72db2f4f92cda61be27b93f671d2
Henric Trotzig <henric.trotzig@gmail.com>
2023-03-30 17:30:57 +0200

    Make manifest generate dynamically
    So that we can inject an env variable as the title.
    
    Ignoring the fact that there are some hardcoded colors in here still.
    Will have to fix that somehow.

---
+++ pages/manifest.json.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+// dummy, it's all handled in getServerSideProps
+const Manifest = () => {};
+
+export async function getServerSideProps({ res }) {
+  const manifest = {
+    name: process.env.NEXT_PUBLIC_TITLE,
+    scope: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#e54e37',
+    icons: [
+      {
+        src: '/app-icon.svg',
+        type: 'image/svg+xml',
+        sizes: '512x512',
+      },
+      {
+        src: '/app-icon-192.png',
+        type: 'image/png',
+        sizes: '192x192',
+      },
+      {
+        src: '/app-icon-512.png',
+        type: 'image/png',
+        sizes: '512x512',
+      },
+    ],
+  };
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify(manifest));
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default Manifest;
