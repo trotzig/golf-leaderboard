017239a9f245c82db4e3e69d9c6e7a2bde4c53bc
Henric Trotzig <henric.trotzig@gmail.com>
2024-02-14 21:30:58 +0100

    Move cron jobs to Vercel
    I've had these on pingdom until now. Going to move them to vercel since
    they now support cron.

--- pages/api/cron/sync-data.js
+++ pages/api/cron/sync-data.js
@@ -1,17 +1,6 @@
 import syncData from '../../../src/syncData.mjs';
 
-const { CRON_AUTH_KEY, BASE_URL } = process.env;
-
 export default async function handler(req, res) {
-  if (!/localhost/.test(BASE_URL)) {
-    if (req.method !== 'POST') {
-      return res.status(400).send('This endpoint accepts POST requests');
-    }
-    const { authToken } = req.body;
-    if (authToken !== CRON_AUTH_KEY) {
-      return res.status(401).send('Invalid credentials');
-    }
-  }
   await syncData({ full: false });
 
   res.status(200).send('Cron job executed successfully');
